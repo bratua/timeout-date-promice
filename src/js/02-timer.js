2a1d6e48e12ce642a65fca767443557741b7648f
Sergey Lisoviy <serlipaua@gmail.com>
2022-08-07 17:53:12 +0300

    refactoring timer to class model

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,16 +2,36 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 import '../css/common.css';
+import Timer from './22-timer-class.js';
+
+const ref = {
+  inputDateField: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  pauseBtn: document.querySelector('button[data-pause]'),
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
 let timerId = null;
 let actionTime = 0;
 let timerIsOn = false;
 const DELAY = 1000;
+let testTimer = null;
 const BTN_STATE = {
   start: 'Start count',
   stop: 'Stop count',
 };
 
+ref.startBtn.addEventListener('click', onBtnStartClick);
+ref.pauseBtn.addEventListener('click', onBtnPauseClick);
+ref.startBtn.textContent = BTN_STATE.start;
+
+const timer = new Timer({ setTimerFn: setTimer });
+
+//! flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -29,81 +49,36 @@ const options = {
     }
 
     actionTime = futureDate - currentDate;
-    console.log('actionTime ', actionTime);
-    console.log(convertMs(actionTime));
-    setTimer(convertMs(actionTime));
+    timer.timeMs = actionTime;
+    timer.render();
+    // testTimer = new Timer(actionTime);
+    // testTimer.renderTimer();
+    // console.log('actionTime ', actionTime);
+    // console.log(convertMs(actionTime));
+    // setTimer(convertMs(actionTime));
   },
 };
-
-const ref = {
-  inputDateField: document.querySelector('#datetime-picker'),
-  startBtn: document.querySelector('button[data-start]'),
-  timer: document.querySelector('.timer'),
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
-};
-
-ref.startBtn.addEventListener('click', onBtnClick);
-ref.startBtn.textContent = BTN_STATE.start;
-
 flatpickr(ref.inputDateField, options);
+//! flatpickr
 
-function onBtnClick() {
-  console.log('btn click');
-  if (timerIsOn) {
-    stopTimer();
-    timerIsOn = false;
+function onBtnStartClick() {
+  if (timer.timerIsOn) {
+    timer.stop();
+    ref.startBtn.textContent = BTN_STATE.start;
     return;
   }
-
-  runTimer();
-  timerIsOn = true;
-}
-
-function stopTimer() {
-  ref.startBtn.textContent = BTN_STATE.start;
-  clearInterval(timerId);
-  setTimer(convertMs(actionTime));
-}
-
-function runTimer() {
+  timer.start();
   ref.startBtn.textContent = BTN_STATE.stop;
-  timerId = setInterval(decTime, DELAY);
 }
 
-function decTime() {
-  actionTime -= 1000;
-
-  if (actionTime <= 0) {
-    stopTimer();
-    actionTime = 0;
-    setTimer(convertMs(actionTime));
+function onBtnPauseClick() {
+  if (timer.timerIsOn) {
+    timer.pause();
+    ref.startBtn.textContent = BTN_STATE.start;
     return;
   }
-
-  setTimer(convertMs(actionTime));
-  console.log('left ms ', actionTime);
-}
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+  timer.start();
+  ref.startBtn.textContent = BTN_STATE.stop;
 }
 
 function setTimer(timerData) {
@@ -111,3 +86,55 @@ function setTimer(timerData) {
     ref[key].textContent = timerData[key].toString().padStart(2, 0);
   }
 }
+
+// function onBtnClick() {
+//   if (timerIsOn) {
+//     stopTimer();
+
+//     return;
+//   }
+
+//   runTimer();
+// }
+
+// function stopTimer() {
+//   ref.startBtn.textContent = BTN_STATE.start;
+//   clearInterval(timerId);
+//   timerIsOn = false;
+//   setTimer(convertMs(actionTime));
+// }
+
+// function runTimer() {
+//   ref.startBtn.textContent = BTN_STATE.stop;
+//   timerId = setInterval(decTime, DELAY);
+//   timerIsOn = true;
+// }
+
+// function decTime() {
+//   actionTime -= 1000;
+
+//   if (actionTime <= 0) {
+//     stopTimer();
+//     actionTime = 0;
+//     setTimer(convertMs(actionTime));
+//     return;
+//   }
+
+//   setTimer(convertMs(actionTime));
+//   // console.log('left ms ', actionTime);
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = Math.floor(ms / day); // Remaining days
+//   const hours = Math.floor((ms % day) / hour); // Remaining hours
+//   const minutes = Math.floor(((ms % day) % hour) / minute); // Remaining minutes
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second); // Remaining seconds
+
+//   return { days, hours, minutes, seconds };
+// }
---
+++ src/js/22-timer-class.js
@@ -0,0 +1,65 @@
+export default class Timer {
+  constructor({ setTimerFn }) {
+    this.ms = 0;
+    this.delay = 100;
+    this.timerIsOn = false;
+    this.timerId = null;
+    this.setTimerFn = setTimerFn;
+  }
+
+  render() {
+    this.setTimerFn(this.convertMs(this.timeMs));
+    //  console.log('render', this);
+  }
+
+  get timeMs() {
+    return this.ms;
+  }
+
+  set timeMs(ms) {
+    this.ms = ms;
+  }
+
+  start() {
+    console.log('start');
+    this.timerIsOn = true;
+    this.timerId = setInterval(() => {
+      if (this.timeMs <= 0) {
+        this.stop();
+        return;
+      }
+      this.timeMs -= 1000;
+      this.render();
+    }, this.delay);
+  }
+
+  stop() {
+    console.log('stop');
+    this.timerIsOn = false;
+    this.timeMs = 0;
+    this.render();
+    clearInterval(this.timerId);
+  }
+
+  pause() {
+    console.log('pause');
+    this.timerIsOn = false;
+    this.render();
+    clearInterval(this.timerId);
+  }
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day); // Remaining days
+    const hours = Math.floor((ms % day) / hour); // Remaining hours
+    const minutes = Math.floor(((ms % day) % hour) / minute); // Remaining minutes
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second); // Remaining seconds
+
+    return { days, hours, minutes, seconds };
+  }
+}
